7b3cee52295773fd45be47e9c41c1aa0f5bffabe
John Paul Naag <122628970+kuyajp123@users.noreply.github.com>
2026-08-31 11:55:23 +0800

    chore: local configuration setup
    * chore: create staging environment
    
    * chore: add error logging
    
    * chore: add security advisory migration file
    
    * chore: update env example
    
    * ci: add e2e test and remove yml workflow test file
    
    * test: add migration test
    
    * ci: add e2e test and remove yml workflow test file

--- resources/js/routes/inquiry/index.ts
+++ resources/js/routes/inquiry/index.ts
@@ -1,61 +1,72 @@
-import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition } from './../../wayfinder'
+import {
+    queryParams,
+    type RouteQueryOptions,
+    type RouteDefinition,
+    type RouteFormDefinition,
+} from './../../wayfinder';
 /**
-* @see \App\Http\Controllers\Public\InquiryController::__invoke
+ * @see \App\Http\Controllers\Public\InquiryController::__invoke
  * @see app/Http/Controllers/Public/InquiryController.php:13
  * @route '/inquiry'
  */
-export const submit = (options?: RouteQueryOptions): RouteDefinition<'post'> => ({
+export const submit = (
+    options?: RouteQueryOptions,
+): RouteDefinition<'post'> => ({
     url: submit.url(options),
     method: 'post',
-})
+});
 
 submit.definition = {
-    methods: ["post"],
+    methods: ['post'],
     url: '/inquiry',
-} satisfies RouteDefinition<["post"]>
+} satisfies RouteDefinition<['post']>;
 
 /**
-* @see \App\Http\Controllers\Public\InquiryController::__invoke
+ * @see \App\Http\Controllers\Public\InquiryController::__invoke
  * @see app/Http/Controllers/Public/InquiryController.php:13
  * @route '/inquiry'
  */
 submit.url = (options?: RouteQueryOptions) => {
-    return submit.definition.url + queryParams(options)
-}
+    return submit.definition.url + queryParams(options);
+};
 
 /**
-* @see \App\Http\Controllers\Public\InquiryController::__invoke
+ * @see \App\Http\Controllers\Public\InquiryController::__invoke
  * @see app/Http/Controllers/Public/InquiryController.php:13
  * @route '/inquiry'
  */
 submit.post = (options?: RouteQueryOptions): RouteDefinition<'post'> => ({
     url: submit.url(options),
     method: 'post',
-})
+});
 
-    /**
-* @see \App\Http\Controllers\Public\InquiryController::__invoke
+/**
+ * @see \App\Http\Controllers\Public\InquiryController::__invoke
  * @see app/Http/Controllers/Public/InquiryController.php:13
  * @route '/inquiry'
  */
-    const submitForm = (options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
-        action: submit.url(options),
-        method: 'post',
-    })
+const submitForm = (
+    options?: RouteQueryOptions,
+): RouteFormDefinition<'post'> => ({
+    action: submit.url(options),
+    method: 'post',
+});
 
-            /**
-* @see \App\Http\Controllers\Public\InquiryController::__invoke
+/**
+ * @see \App\Http\Controllers\Public\InquiryController::__invoke
  * @see app/Http/Controllers/Public/InquiryController.php:13
  * @route '/inquiry'
  */
-        submitForm.post = (options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
-            action: submit.url(options),
-            method: 'post',
-        })
-    
-    submit.form = submitForm
+submitForm.post = (
+    options?: RouteQueryOptions,
+): RouteFormDefinition<'post'> => ({
+    action: submit.url(options),
+    method: 'post',
+});
+
+submit.form = submitForm;
 const inquiry = {
     submit: Object.assign(submit, submit),
-}
+};
 
-export default inquiry
+export default inquiry;
