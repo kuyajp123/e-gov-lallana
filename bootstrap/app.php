5aea7fc29d868a45ba7e5f9d85dc9dcba46232db
John Paul Naag <122628970+kuyajp123@users.noreply.github.com>
2026-09-18 21:11:46 +0800

    fix(deploy): remove Link preload headers and tune nginx keepalive for http2

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,7 +6,6 @@ use App\Http\Middleware\SetLocale;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 use Illuminate\Http\Request;
 
 $app = Application::configure(basePath: dirname(__DIR__))
@@ -25,7 +24,6 @@ $app = Application::configure(basePath: dirname(__DIR__))
             SetLocale::class,
             HandleAppearance::class,
             HandleInertiaRequests::class,
-            AddLinkHeadersForPreloadedAssets::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
